a50644fbb86ca87188175be571f2cfd53875e386
Christopher Martin <cgmartin@gmail.com>
2017-05-27 14:59:47 -0400

    use if block instead of return

--- lib/searchyll.rb
+++ lib/searchyll.rb
@@ -6,38 +6,40 @@ require "searchyll/configuration"
 require "searchyll/indexer"
 require "nokogiri"
 
-if Jekyll.env != 'production'
+if Jekyll.env == 'production'
+  begin
+    indexers = {}
+
+    Jekyll::Hooks.register(:site, :pre_render) do |site|
+      config = Searchyll::Configuration.new(site)
+      indexers[site] = Searchyll::Indexer.new(config)
+      indexers[site].start
+    end
+
+    Jekyll::Hooks.register :site, :post_render do |site|
+      indexers[site].finish
+    end
+
+    Jekyll::Hooks.register :posts, :post_render do |post|
+      # strip html
+      nokogiri_doc = Nokogiri::HTML(post.output)
+
+      puts %(indexing #{post.url})
+
+      indexer = indexers[post.site]
+      indexer << post.data.merge({
+        id:     post.id,
+        url:    post.url,
+        text:   nokogiri_doc.xpath("//article//text()").to_s.gsub(/\s+/, " ")
+      })
+    end
+
+  rescue => e
+    puts e.message
+  end
+
+else
   puts "searchyll: skipping processing for a non-production build"
-  return
 end
 
-begin
-  indexers = {}
 
-  Jekyll::Hooks.register(:site, :pre_render) do |site|
-    config = Searchyll::Configuration.new(site)
-    indexers[site] = Searchyll::Indexer.new(config)
-    indexers[site].start
-  end
-
-  Jekyll::Hooks.register :site, :post_render do |site|
-    indexers[site].finish
-  end
-
-  Jekyll::Hooks.register :posts, :post_render do |post|
-    # strip html
-    nokogiri_doc = Nokogiri::HTML(post.output)
-
-    puts %(indexing #{post.url})
-
-    indexer = indexers[post.site]
-    indexer << post.data.merge({
-      id:     post.id,
-      url:    post.url,
-      text:   nokogiri_doc.xpath("//article//text()").to_s.gsub(/\s+/, " ")
-    })
-  end
-
-rescue => e
-  puts e.message
-end
